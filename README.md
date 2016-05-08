9e5c9dd74b5ee6c02f04361bfc5c28dbcf183693
stephenpardy <stephen.pardy@gmail.com>
2016-05-08 15:19:24 -0500

    words

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # scopr
-Ham-fisted Scoping in Python
+Ham-fisted scoping in Python
+
+Load it into your project and enjoy scopes anywhere:
+    from scopr import scope
+
+    a = 1  # define a variable outside the scope
+    with scope():
+        a = 2  # redefine and use a variable here
+
+    print(a)  # prints 1
+
+
+Works on CPython with Python 2 3, but is mostly untested for edge-cases.
--- scopr.py
+++ scopr.py
@@ -5,11 +5,15 @@ import inspect
 def scope():
     curframe = inspect.currentframe()
     calframe = inspect.getouterframes(curframe, 1)
-    glbs = dict(calframe[2][0].f_globals)
+
+    glbs = dict(calframe[2][0].f_globals)  # hold on to the old globals
     yield
     new_variables = set(glbs) ^ set(calframe[2][0].f_globals)
+
+    # rewrite the global variables
     for k, v in glbs.items():
         calframe[2][0].f_globals[k] = v
 
+    #delete any newly-set variables
     for variable in new_variables:
         del calframe[2][0].f_globals[variable]
